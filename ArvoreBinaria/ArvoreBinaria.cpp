11469a119d5fba54970a1e507bd2c2cf26b2ee52
Nathália Regina <110910030+nathreginavt@users.noreply.github.com>
2022-10-25 23:05:10 -0300

    Update ArvoreBinaria.cpp

--- ArvoreBinaria/ArvoreBinaria.cpp
+++ ArvoreBinaria/ArvoreBinaria.cpp
@@ -71,7 +71,7 @@ void menu()
 void inicializar()
 {
 
-	// provisÛrio porque n„o libera a memoria usada pela arvore
+	// provis√≥rio porque n√£o libera a memoria usada pela arvore
 	NO* raiz = NULL;
 	
 	cout << "Arvore inicializada \n";
@@ -121,7 +121,21 @@ NO* criaNO(int valor)
 
 NO* insereArvore(NO* no, int valor)
 {
-	
+	if (no == NULL)
+	{
+	     no = criaNO(valor);
+	}
+	else 
+        {
+		if (valor < no->valor)
+		{
+			return insereArvore(no->esq, valor);
+		}
+		else
+		{
+			return insereArvore(no->dir, valor);
+		}
+	}
 }
 
 int elementosArvore(NO* no)
@@ -135,5 +149,11 @@ int elementosArvore(NO* no)
 
 void exibirElementosArvore(NO* no)
 {
-	
+	if (no == NULL){
+	    cout << no->esq << endl;
+	}
+	else{
+    	exibirElementosArvore(no->esq);
+	    exibirElementosArvore(no->dir);
+	}
 }
